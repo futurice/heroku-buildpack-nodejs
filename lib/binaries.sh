2974b95b3f8035961d93a58946ea830d20d5396a
Jussi Moisio <jussi.moisio@futurice.com>
2015-11-19 22:11:54 +0200

    First test

--- lib/binaries.sh
+++ lib/binaries.sh
@@ -60,3 +60,12 @@ install_npm() {
     fi
   fi
 }
+
+install_aws() {
+  python --version
+  curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+  unzip awscli-bundle.zip
+  ./awscli-bundle/install -b ~/bin/aws
+  export PATH=~/bin:$PATH
+  aws help
+}
