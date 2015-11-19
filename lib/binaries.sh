23da64e61f4dc191248d018333eb9640217c236a
Jussi Moisio <jussi.moisio@futurice.com>
2015-11-19 22:21:13 +0200

    Removed extra prints in AWS installation.

--- lib/binaries.sh
+++ lib/binaries.sh
@@ -62,10 +62,8 @@ install_npm() {
 }
 
 install_aws() {
-  python --version
   curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
   unzip awscli-bundle.zip
   ./awscli-bundle/install -b ~/bin/aws
   export PATH=~/bin:$PATH
-  aws help
 }
